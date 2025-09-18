5d6ad8008ff74aefe8471ff2dd18cae740c51a75
Juan Carlos <montilla269@gmail.com>
2025-09-19 00:15:28 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',      (req, res) => res.send('Hola root!'));
+app.get('/ping',  (req, res) => res.send('pong'));
+app.get('/echo', (req, res) => res.send('Simple Express multi-stage API'));
+
+app.listen(port, () => {
+  console.log(`Servidor en http://localhost:${port}`);
+});
